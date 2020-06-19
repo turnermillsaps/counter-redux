7db4ba368863bdc3712854759a98662d28aff6e6
Turner Millsaps <turnermillsaps.plp@gmail.com>
2020-06-19 00:29:25 -0400

    First and final commit with working counter

---
+++ src/components/Counter.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Counter extends Component {
+
+    increment = () => {
+        this.props.increment()
+    }
+
+    decrement = () => {
+        this.props.decrement()
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Counter</h2>
+                <div>
+                    <button onClick={this.decrement}>-</button>
+                        <span>{this.props.count}</span>
+                    <button onClick={this.increment}>+</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        count: state.count
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        increment: function() {
+            dispatch({ type: 'INCREMENT' })
+        },
+        decrement: function() {
+            dispatch({ type: 'DECREMENT' })
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Counter from './components/Counter';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+// Store setup
+const initialState = {
+  count: 0
+}
+
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Counter />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
